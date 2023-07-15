2ff1bca6226186dcf8f3e49ecf2383143a9d2fe3
peteruche21 <anyaogupeter601@gmail.com>
2023-07-15 18:41:13 +0100

    chore: add avatar

---
+++ components/UI/Avatar.tsx
@@ -0,0 +1,24 @@
+import { formatAddress } from "@/utils/addresses";
+import Image from "next/image";
+import { useEffect, useState } from "react";
+
+const Avatar = ({ address }: { address: string }) => {
+  const [avatar, setAvatar] = useState<string>("");
+  const getAvatar = async () => {
+    setAvatar((await formatAddress(address, true)).avatar);
+  };
+
+  useEffect(() => {
+    getAvatar();
+  }, [address]);
+
+  return (
+    <label tabIndex={0} className="btn btn-ghost btn-circle avatar">
+      <div className="w-7 rounded-full">
+        <Image src={avatar || "/metamask.png"} width={30} height={30} alt="" />
+      </div>
+    </label>
+  );
+};
+
+export default Avatar;

0f4fb1db78b75f32070789bc32b4b5080d754c7f
peteruche21 <anyaogupeter601@gmail.com>
2023-07-15 17:55:35 +0100

    chore: setup eth addresses utilities

---
+++ utils/addresses.ts
@@ -0,0 +1,14 @@
+import { cache } from "react";
+import { JsonRpcProvider } from "ethers";
+
+export const formatAddress = cache(async (address: string, getName?: boolean) => {
+  if (address.length !== 42) return;
+  const provider = new JsonRpcProvider(process.env.SEPOLIA_RPC_URL);
+  const name = getName && (await provider?.lookupAddress(address));
+  return {
+    address,
+    shortString: `${address.slice(0, 6)}...${address.slice(-4)}`,
+    name,
+    avatar: `https://effigy.im/a/${name || address}.svg`,
+  };
+});
